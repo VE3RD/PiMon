c7396840dff95a189ead10bffb9617179244f4f4
phil <ve3rd@rac.ca>
2022-01-18 19:43:40 -0500

    updated

--- getstripped2.sh
+++ getstripped2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 rm /usr/local/etc/stripped2.*
-rm /home/pi-star/Scripts/stripped2.*
-wget https://raw.githubusercontent.com/EA7KDO/Scripts/master/stripped2.csv /home/pi-star/Scripts/stripped2.csv
-cp /home/pi-star/Scripts/stripped2.csv /usr/local/etc/
+wget https://raw.githubusercontent.com/EA7KDO/Scripts/master/stripped2.csv -O /usr/local/etc/stripped2.csv
 
